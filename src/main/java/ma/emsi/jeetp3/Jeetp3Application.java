cdb5920a550c5b50bc3931e5ac9be99adec78c01
khoumaniasmaa <khoumaniasmaa@gmail.com>
2022-04-14 10:30:53 +0000

    first commit

--- src/main/java/ma/emsi/jeetp3/Jeetp3Application.java
+++ src/main/java/ma/emsi/jeetp3/Jeetp3Application.java
@@ -14,16 +14,16 @@ public class Jeetp3Application  {
     public static void main(String[] args) {
         SpringApplication.run(Jeetp3Application.class, args);
     }
-    @Bean
+    //@Bean
     CommandLineRunner commandLineRunner(PatientRepository patientRepository){
         return args -> {patientRepository.save(
-                new Patient(null,"Asmaa",new Date(),false,12));
+                new Patient(null,"Asmaa",new Date(),false,112));
             patientRepository.save(
                     new Patient(null,"hayat",new Date(),true,321));
             patientRepository.save(
-                    new Patient(null,"janat",new Date(),false,65));
+                    new Patient(null,"janat",new Date(),false,165));
             patientRepository.save(
-                    new Patient(null,"hanae",new Date(),true,32));
+                    new Patient(null,"hanae",new Date(),true,132));
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
             });
--- src/main/java/ma/emsi/jeetp3/web/PatientController.java
+++ src/main/java/ma/emsi/jeetp3/web/PatientController.java
@@ -3,21 +3,25 @@ package ma.emsi.jeetp3.web;
 import lombok.AllArgsConstructor;
 import ma.emsi.jeetp3.entities.Patient;
 import ma.emsi.jeetp3.repositories.PatientRepository;
+import org.hibernate.boot.jaxb.spi.Binding;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
 import java.awt.print.Pageable;
 import java.util.List;
 @Controller
 @AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
-    @GetMapping (path="/index") /*nom de pages*/
+    @GetMapping (path="/user/index")
    public String patients(Model model ,
                           @RequestParam(name ="page",defaultValue = "0") int page,
                           @RequestParam(name ="size",defaultValue = "5")int size,
@@ -30,18 +34,40 @@ public class PatientController {
         model.addAttribute("keyword",keyword);
      return "patients";
    }
-   @GetMapping("/delete")
+   @GetMapping("/admin/delete")
    public String delete(Long id,String keyword,int page){
        patientRepository.deleteById(id);
-       return "redirect:/index?page"+page+"&keyword"+keyword;
+       return "redirect:/user/index?page"+page+"&keyword"+keyword;
    }
    @GetMapping("/")
     public String home(){
-       return "redirect:/index";
+       return "home";
    }
-   @GetMapping("/patients")
+   @GetMapping("/user/patients")
    @ResponseBody
    public List<Patient>listPatients(){
        return patientRepository.findAll();
    }
+
+   @GetMapping("/admin/formPatients")
+   public String formPatients(Model model){
+       model.addAttribute("patient",new Patient());
+       return "formPatients";
+}
+@PostMapping(path = "/admin/save")
+public  String save (Model model, @Valid Patient patient, BindingResult bindingResult,
+                     @RequestParam(defaultValue="")String keyword, @RequestParam(defaultValue="0") int page){
+       if (bindingResult.hasErrors()) return "formPatients";
+       patientRepository.save(patient);
+       return "redirect:/user/index?page="+page+"&keyword="+keyword;
+}
+    @GetMapping(path = "/admin/editPatient")
+    public  String editPatient(Model model,long id , String keyword, int page){
+       Patient patient=patientRepository.findById(id).orElse(null);
+        if (patient==null)throw  new RuntimeException("Patient introuvable");
+        model.addAttribute("patient",patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
+    }
 }
