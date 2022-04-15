de92720815ce67be99a68952d2bc1c30d1229ceb
khoumaniasmaa <khoumaniasmaa@gmail.com>
2022-04-15 23:41:46 +0000

    In Memory Authentification

--- src/main/java/ma/emsi/jeetp3/sec/SecurityConfig.java
+++ src/main/java/ma/emsi/jeetp3/sec/SecurityConfig.java
@@ -3,10 +3,14 @@ package ma.emsi.jeetp3.sec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.CachingUserDetailsService;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,22 +23,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+/* memory authentification*/
         PasswordEncoder passwordEncoder=passwordEncoder();
-/*
+
         String encodePWD=passwordEncoder.encode("1234");
          System.out.println(encodePWD);
         auth.inMemoryAuthentication().withUser("user1").password(encodePWD).roles("USER");
         auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder().encode("1111")).roles("USER");
         auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("2345")).roles("USER","ADMIN");
-*/
-        /*jdbcauth*/
+
+        /*jdbcauth
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .usersByUsernameQuery("select username as principal ,password as credentials,active from users where username=?")
                 .authoritiesByUsernameQuery(" select username as principal ,role as role from users_roles where username=?")
                 .rolePrefix("ROLE_")
                 .passwordEncoder(passwordEncoder);
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return null;
+            }
+        });*/
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -42,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeHttpRequests().antMatchers("/").permitAll();
         http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN");
         http.authorizeHttpRequests().antMatchers("/user/**").hasRole("USER");
+        http.authorizeHttpRequests().antMatchers("/webjars/**").permitAll();
         http.authorizeHttpRequests().anyRequest().authenticated();
         http.exceptionHandling().accessDeniedPage("/403");
     }
