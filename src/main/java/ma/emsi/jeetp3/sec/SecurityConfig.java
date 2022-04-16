2a0b76e71025fe34d7f814b67402e0e43105dff2
khoumaniasmaa <khoumaniasmaa@gmail.com>
2022-04-16 00:59:57 +0000

    JDBC Authentification

--- src/main/java/ma/emsi/jeetp3/sec/SecurityConfig.java
+++ src/main/java/ma/emsi/jeetp3/sec/SecurityConfig.java
@@ -26,20 +26,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 /* memory authentification*/
         PasswordEncoder passwordEncoder=passwordEncoder();
 
-        String encodePWD=passwordEncoder.encode("1234");
+       /* String encodePWD=passwordEncoder.encode("1234");
          System.out.println(encodePWD);
         auth.inMemoryAuthentication().withUser("user1").password(encodePWD).roles("USER");
         auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder().encode("1111")).roles("USER");
         auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("2345")).roles("USER","ADMIN");
-
-        /*jdbcauth
+*/
+        /*jdbcauth*/
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .usersByUsernameQuery("select username as principal ,password as credentials,active from users where username=?")
                 .authoritiesByUsernameQuery(" select username as principal ,role as role from users_roles where username=?")
                 .rolePrefix("ROLE_")
                 .passwordEncoder(passwordEncoder);
-        auth.userDetailsService(new UserDetailsService() {
+        /*auth.userDetailsService(new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
                 return null;
